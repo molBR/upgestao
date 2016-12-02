2e46b1a145d8997aa4882d0cb32a0f7378913dfc
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2016-12-01 23:34:00 -0200

    retirei a funcao do botao inserir porque estava dando conflito

--- cadastro_produto_maior.py
+++ cadastro_produto_maior.py
@@ -1,14 +1,13 @@
 from Tkinter import *
-import cadastro_produto_menor as cpm
-root=Tk()
 
 # funcao para teste
-
 def Teste():
     print("Testado")
+# fim
+
+root=Tk()
 
 # menu principal
-
 menu = Menu(root)
 root.config(menu=menu)
 
@@ -21,6 +20,7 @@ subMenu1.add_command(label="Sair", command=Teste)
 
 subMenu2 = Menu(menu)
 menu.add_cascade(label="Editar", menu=subMenu2)
+subMenu2.add_command(label="Copiar Tudo", command=Teste)
 subMenu2.add_command(label="Copiar", command=Teste)
 subMenu2.add_command(label="Colar", command=Teste)
 subMenu2.add_command(label="Cortar", command=Teste)
@@ -29,9 +29,9 @@ subMenu3 = Menu(menu)
 menu.add_cascade(label="Visualizar", menu=subMenu3)
 subMenu3.add_command(label="Ferramentas", command=Teste)
 subMenu3.add_command(label="Arquivo Recente", command=Teste)
+# fim
 
 # abas de opcoes
-
 cor1 = '#D32F2F'
 
 toolbar = Frame(root, bg=cor1)
@@ -39,152 +39,186 @@ toolbar = Frame(root, bg=cor1)
 opcao1 = Button(toolbar, text="   Doces   ", bg=cor1,  command=Teste)
 opcao1['font']=('bold')
 opcao1['fg']='white'
-opcao1.pack(side=LEFT, padx=2, pady=2)
+opcao1.pack(side=LEFT, padx=1, pady=1)
 opcao2 = Button(toolbar, text="Salgados", bg=cor1, command=Teste)
 opcao2['font']=('bold')
 opcao2['fg']='white'
-opcao2.pack(side=LEFT, padx=2, pady=2)
+opcao2.pack(side=LEFT, padx=1, pady=1)
 opcao3 = Button(toolbar, text="  Massas  ", bg=cor1, command=Teste)
 opcao3['font']=('bold')
 opcao3['fg']='white'
-opcao3.pack(side=LEFT, padx=2, pady=2)
+opcao3.pack(side=LEFT, padx=1, pady=1)
 opcao4 = Button(toolbar, text="  Bebidas  ", bg=cor1, command=Teste)
 opcao4['font']=('bold')
 opcao4['fg']='white'
-opcao4.pack(side=LEFT, padx=2, pady=2)
+opcao4.pack(side=LEFT, padx=1, pady=1)
 opcao5 = Button(toolbar, text="   Outros   ", bg=cor1, command=Teste)
 opcao5['font']=('bold')
 opcao5['fg']='white'
-opcao5.pack(side=LEFT, padx=2, pady=2)
+opcao5.pack(side=LEFT, padx=1, pady=1)
 
 toolbar.pack(side=TOP, fill=X)
+# fim
 
 # barra de 'status'
-
-status = Label(root, text="Estado: Normal", bg="white", bd=1, relief=SUNKEN, anchor=W)
+status = Label(root, text="Estado: Rodando", bg="white", bd=1, relief=SUNKEN, anchor=W)
 status.pack(side=BOTTOM, fill=X)
+# fim
 
-# informacoes gerais
+class Example(Frame):
+    def __init__(self, root):
 
-class Janela:
-    def __init__(self,instancia_Tk):
+        self.container1 = Frame(root)
+        self.container2 = Frame(root)
+        self.container1.pack(fill=X)
+        self.container2.pack(side=BOTTOM, fill=X, padx=15, pady=15)
 
-        instancia_Tk.title('Programa Guts')
-
-        self.container1 = Frame(instancia_Tk)
-        self.container2 = Frame(instancia_Tk)
-        self.container3 = Frame(instancia_Tk)
-        self.container4 = Frame(instancia_Tk)
-        self.container5 = Frame(instancia_Tk)
-        self.container6 = Frame(instancia_Tk)
-        self.container7 = Frame(instancia_Tk)
-        self.container8 = Frame(instancia_Tk)
-        self.container9 = Frame(instancia_Tk)
-        self.container10 = Frame(instancia_Tk)
-        self.container11 = Frame(instancia_Tk)
-        self.container12 = Frame(instancia_Tk)
-        self.container13 = Frame(instancia_Tk)
-        self.container14 = Frame(instancia_Tk)
-        self.container15 = Frame(instancia_Tk)
-        self.container16 = Frame(instancia_Tk)
-        self.container17 = Frame(instancia_Tk)
-        self.container18 = Frame(instancia_Tk)
-        self.container1.pack()
-        self.container2.pack()
-        self.container3.pack()
-        self.container4.pack()
-        self.container5.pack()
-        self.container6.pack()
-        self.container7.pack()
-        self.container8.pack()
-        self.container9.pack()
-        self.container10.pack()
-        self.container11.pack()
-        self.container12.pack()
-        self.container13.pack()
-        self.container14.pack()
-        self.container15.pack()
-        self.container16.pack()
-        self.container17.pack()
-        self.container18.pack()
-
-        self.objeto1 = Label(self.container1, text="Codigo:")
-        self.objeto1["font"]=["bold"]
+#"cabecalho" da tabela dos itens
+        self.objeto1 = Label(self.container1, text="       Codigo:")
+        self.objeto1["font"] = ["bold"]
         self.objeto1.pack(side=LEFT)
-        self.espaco1 = Label(self.container1, text="                                               ")
-        self.espaco1.pack(side=LEFT)
-        self.objeto2 = Label(self.container1, text="Nome:")
-        self.objeto2["font"]=["bold"]
+        self.objeto2 = Label(self.container1, text="                                       ")
+        self.objeto2["font"] = ["bold"]
         self.objeto2.pack(side=LEFT)
-        self.espaco2 = Label(self.container1, text="                                               ")
-        self.espaco2.pack(side=LEFT)
-        self.espaco3 = Label(self.container1, text="                                               ")
-        self.espaco3.pack(side=LEFT)
-        self.objeto3 = Label(self.container1, text="Valor:")
-        self.objeto3["font"]=["bold"]
+        self.objeto3 = Label(self.container1, text="Nome:")
+        self.objeto3["font"] = ["bold"]
         self.objeto3.pack(side=LEFT)
-        self.espaco4 = Label(self.container1, text="                                               ")
-        self.espaco4.pack(side=LEFT)
-
-        self.salto1 = Label(self.container2, text="")
-        self.salto1.pack()
-        self.salto2 = Label(self.container3, text="")
-        self.salto2.pack()
-        self.salto3 = Label(self.container4, text="")
-        self.salto3.pack()
-        self.salto4 = Label(self.container5, text="")
-        self.salto4.pack()
-        self.salto5 = Label(self.container6, text="")
-        self.salto5.pack()
-        self.salto6 = Label(self.container7, text="")
-        self.salto6.pack()
-        self.salto7 = Label(self.container8, text="")
-        self.salto7.pack()
-        self.salto8 = Label(self.container9, text="")
-        self.salto8.pack()
-        self.salto9 = Label(self.container10, text="")
-        self.salto9.pack()
-        self.salto10 = Label(self.container11, text="")
-        self.salto10.pack()
-        self.salto11 = Label(self.container12, text="")
-        self.salto11.pack()
-        self.salto12 = Label(self.container13, text="")
-        self.salto12.pack()
-        self.salto13 = Label(self.container14, text="")
-        self.salto13.pack()
-        self.salto14 = Label(self.container15, text="")
-        self.salto14.pack()
-        self.salto15 = Label(self.container16, text="")
-        self.salto15.pack()
-
-        self.objeto4 = Button(self.container17, text="Inserir",command=cpm.Help) 
-        self.objeto4["font"] = ("Arial", "10")
-        self.objeto4['padx'] = 10
+        self.objeto4 = Label(self.container1, text="                                              ")
+        self.objeto4["font"] = ["bold"]
         self.objeto4.pack(side=LEFT)
-        self.espaco5 = Label(self.container17, text="    ")
-        self.espaco5.pack(side=LEFT)
-        self.objeto5 = Button(self.container17, text="Editar")
-        self.objeto5["font"] = ("Arial", "10")
-        self.objeto5['padx'] = 10
+        self.objeto5 = Label(self.container1, text="Valor:")
+        self.objeto5["font"] = ["bold"]
         self.objeto5.pack(side=LEFT)
-        self.espaco6 = Label(self.container17, text="    ")
-        self.espaco6.pack(side=LEFT)
-        self.objeto6 = Button(self.container17, text="Excluir")
-        self.objeto6["font"] = ("Arial", "10")
-        self.objeto6['padx'] = 10
+        self.objeto6 = Label(self.container1, text="                                              ")
+        self.objeto6["font"] = ["bold"]
         self.objeto6.pack(side=LEFT)
-        self.espaco7 = Label(self.container17, text="    ")
-        self.espaco7.pack(side=LEFT)
-        self.objeto7 = Label(self.container17, text="Pesquisar:")
-        self.objeto7["font"] = ("Arial", "10")
-        self.objeto7.pack(side=LEFT)
-        self.objeto8 = Entry(self.container17)
-        self.objeto8["width"] = 25
-        self.objeto8["font"] = ("Arial", "10")
-        self.objeto8.pack(side=LEFT)
+# fim
 
-Janela(root)
-##root.iconbitmap(r'icon.ico') # so funciona se a imagem estiver dentro dessa pasta
+# espaco com "inserir", "editar", "excluir" e "pesquisar"
+
+        self.espaco1 = Label(self.container2, text="             ")
+        self.espaco1.pack(side=LEFT)
+        self.inserir = Button(self.container2, text="Inserir", command=Teste)
+        self.inserir["font"] = ("Arial", "10")
+        self.inserir['padx'] = 10
+        self.inserir.pack(side=LEFT)
+        self.espaco2 = Label(self.container2, text="             ")
+        self.espaco2.pack(side=LEFT)
+        self.editar = Button(self.container2, text="Editar", command=Teste)
+        self.editar["font"] = ("Arial", "10")
+        self.editar['padx'] = 10
+        self.editar.pack(side=LEFT)
+        self.espaco2 = Label(self.container2, text="             ")
+        self.espaco2.pack(side=LEFT)
+        self.excluir = Button(self.container2, text="Excluir", command=Teste)
+        self.excluir["font"] = ("Arial", "10")
+        self.excluir['padx'] = 10
+        self.excluir.pack(side=LEFT)
+        self.espaco3 = Label(self.container2, text="             ")
+        self.espaco3.pack(side=LEFT)
+        self.pesquisar1 = Label(self.container2, text="Pesquisar: ")
+        self.pesquisar1["font"] = ("Arial", "10")
+        self.pesquisar1.pack(side=LEFT)
+        self.pesquisar2 = Entry(self.container2)
+        self.pesquisar2["width"] = 25
+        self.pesquisar2["font"] = ("Arial", "10")
+        self.pesquisar2.pack(side=LEFT)
+        pesquisado = self.pesquisar2.get()  # pesquisado = o que foi escrito no "Entry / barra de pesquisa"
+        self.espaco4 = Label(self.container2, text=" ")
+        self.espaco4.pack(side=LEFT)
+        self.ok = Button(self.container2, text="Ok", command=Teste)
+        self.ok["font"] = ("Arial", "10")
+        self.ok['padx'] = 10
+        self.ok.pack(side=LEFT)
+# fim
+
+# tabela dos itens
+        Frame.__init__(self, root)
+        self.canvas = Canvas(root, borderwidth=0, background="#ffffff")
+        self.frame = Frame(self.canvas, background="#f0f0f0")
+        self.vsb = Scrollbar(root, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((4,4), window=self.frame, anchor="nw",
+                                  tags="self.frame")
+
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+
+        self.populate()
+
+    def populate(self):
+        info = 20
+        '''Put in some fake data'''
+        for row in range(info):
+            if row % 2 == 0:
+                cor = '#ffffff'
+                var1 = IntVar()
+                Checkbutton(self.frame, text="", variable=var1, background=cor, command=Teste).grid(row=row, sticky=W)
+                t="codigo"
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=1)
+            else:
+                cor = '#f0f0f0'
+                var1 = IntVar()
+                Checkbutton(self.frame, text="", variable=var1, background=cor).grid(row=row, sticky=W)
+                t = "codigo"
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=1)
+        for row in range(info):
+            if row % 2 == 0:
+                cor = '#ffffff'
+                t = "                                         "
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=2)
+            else:
+                cor = '#f0f0f0'
+                t = ""
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=2)
+        for row in range(info):
+            if row % 2 == 0:
+                cor = '#ffffff'
+                t = "nome "
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=3)
+            else:
+                cor = '#f0f0f0'
+                t = "nome"
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=3)
+        for row in range(info):
+            if row % 2 == 0:
+                cor = '#ffffff'
+                t = "                                               "
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=4)
+            else:
+                cor = '#f0f0f0'
+                t = ""
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=4)
+        for row in range(info):
+            if row % 2 == 0:
+                cor = '#ffffff'
+                t = "R$" "  valor"
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=5)
+            else:
+                cor = '#f0f0f0'
+                t = "R$"  "  valor"
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=5)
+        for row in range(info):
+            if row % 2 == 0:
+                cor = '#ffffff'
+                t = "                                               "
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=6)
+            else:
+                cor = '#f0f0f0'
+                t = ""
+                Label(self.frame, text=t, font="bold", background=cor).grid(row=row, column=6)
+
+
+    def onFrameConfigure(self, event):
+        '''Reset the scroll region to encompass the inner frame'''
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+# fim
+
+Example(root).pack(side="top", fill="both", expand=True)
+root.iconbitmap(r'C:\Python27\DLLs\icon.ico')
+root.title('Programa Guts')
 root.resizable(width=False, height=False)
 root.geometry('750x450')
 root.mainloop()
--- cadastro_produto_menor.py
+++ cadastro_produto_menor.py
@@ -1,85 +1,86 @@
 from Tkinter import *
 
+root=Tk()
+
+cor1 = '#D32F2F'
+
+# funcao para teste
 def Teste():
     print("Testado")
-def Help():
-    root=Tk()
+#fim
 
-    cor1 = '#D32F2F'
-
-    # funcao para teste
-
-
-    # opcoes do droplist
-
-    OPTIONS = [
-        "Tipo de produto",
-        "Doce",
-        "Salgado",
-        "Massa",
-        "Bebida",
-        "Outro"
+# opcoes do droplist
+OPTIONS = [
+    "Tipo de produto",
+    "Doce",
+    "Salgado",
+    "Massa",
+    "Bebida",
+    "Outro"
     ]
+#fim
 
-    # criacao e posicao dos widgets
+# criacao e posicao dos widgets
+info = Frame(root)
+info.grid(sticky=N+S+W+E)
 
-    info = Frame(root)
-    info.grid(sticky=N+S+W+E)
+salto1 = Label(info, text="       ")
+salto1.grid(row=0, column=0)
 
-    salto1 = Label(info, text="       ")
-    salto1.grid(row=0, column=0)
+nome1 = Label(info, text="Nome:")
+nome1['font']=['bold']
+nome1.grid(row=1, column=1, sticky=W)
 
-    objeto1 = Label(info, text="Nome:")
-    objeto1['font']=['bold']
-    objeto1.grid(row=1, column=1, sticky=W)
+nome2 = Entry(info)
+nome2["width"]=40
+nome2.grid(row=2, column=1)
+nome = nome2.get() #pesquisado = o que foi escrito no "Entry / barra de pesquisa"
 
-    objeto2 = Entry(info)
-    objeto2["width"]=40
-    objeto2.grid(row=2, column=1)
+salto2 = Label(info, text="")
+salto2.grid(row=3, column=0)
 
-    salto2 = Label(info, text="")
-    salto2.grid(row=3, column=0)
+valor1 = Label(info, text="Valor:")
+valor1['font']=['bold']
+valor1.grid(row=4, column=1, sticky=W)
 
-    objeto3 = Label(info, text="Valor:")
-    objeto3['font']=['bold']
-    objeto3.grid(row=4, column=1, sticky=W)
+valor2 = Entry(info)
+valor2["width"]=40
+valor2.grid(row=5, column=1)
+valor = valor2.get() #pesquisado = o que foi escrito no "Entry / barra de pesquisa"
 
-    objeto4 = Entry(info)
-    objeto4["width"]=40
-    objeto4.grid(row=5, column=1)
+salto3 = Label(info, text="")
+salto3.grid(row=6, column=0)
 
-    salto3 = Label(info, text="")
-    salto3.grid(row=6, column=0)
+variable = StringVar(info)
+variable.set(OPTIONS[0])
 
-    variable = StringVar(info)
-    variable.set(OPTIONS[0])
+droplist = apply(OptionMenu, (info, variable) + tuple(OPTIONS))
+droplist.grid(row=7, column=1)
 
-    objeto5 = apply(OptionMenu, (info, variable) + tuple(OPTIONS))
-    objeto5.grid(row=7, column=1)
+salto4 = Label(info, text="")
+salto4.grid(row=8, column=0)
 
-    salto4 = Label(info, text="")
-    salto4.grid(row=8, column=0)
+pronto = Button(info, text="Pronto", bg=cor1, bd=3, command=Teste)
+pronto['font']=['bold']
+pronto['fg']='white'
+pronto.grid(row=9, column=1)
 
-    objeto6 = Button(info, text="Pronto", bg=cor1, bd=3, command=Teste)
-    objeto6['font']=['bold']
-    objeto6['fg']='white'
-    objeto6.grid(row=9, column=1)
+salto5 = Label(info, text="")
+salto5.grid(row=10, column=1)
 
-    salto5 = Label(info, text="")
-    salto5.grid(row=10, column=1)
+espaco1 = Label(info, text="       ")
+espaco1.grid(row=10, column=2)
+#fim
 
-    espaco1 = Label(info, text="       ")
-    espaco1.grid(row=10, column=2)
+# barra de "status"
+status = Label(info, text="Estado: Normal", bg="white", bd=1, relief=SUNKEN, anchor=W)
+status.grid(row= 11, column=0, sticky=S+W+E, columnspan=3)
+#fim
 
-    # barra de "status"
-
-    status = Label(info, text="Estado: Normal", bg="white", bd=1, relief=SUNKEN, anchor=W)
-    status.grid(row= 11, column=0, sticky=S+W+E, columnspan=3)
-
-    # formatacao da janela
-
-    root.title('Cadastro do Produto')
-    ##root.iconbitmap(r'c:\Python27\DLLs\icon.ico')
-    root.resizable(width=False, height=False)
-    root.geometry('298x276')
-    root.mainloop()
+# formatacao da janela
+root.title('Cadastro do Produto')
+    #root.iconbitmap(r'c:\Python27\DLLs\icon.ico')
+root.resizable(width=False, height=False)
+root.geometry('298x276')
+root.mainloop()
+#fim
