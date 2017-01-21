e113b96c879a62571c9587ceefa2e415ba768a5a
OptionalLanguor <felmt@hotmail.com>
2017-01-21 00:55:05 -0200

    SQL backup + pequenas alterações

--- database/backup.py
+++ database/backup.py
@@ -10,6 +10,16 @@ class Backup:
     def exportSQL(self):
         stringDados = db.Database(0).selectProduto()
         print stringDados
+        print len(stringDados)
+        print stringDados[0]
+        print stringDados[0][0]
+        print stringDados[0][1]
+        print stringDados[0][2]
+        print stringDados[0][3]
+        print stringDados[0][4]
+
+        print stringDados[1]
+        print stringDados[84]
         # db.insertProduto(prod1)
 
     #Método que insere dados ao sistema ao ler um arquivo com código SQL
