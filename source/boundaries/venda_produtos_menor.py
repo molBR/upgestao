3d1daf6ac4247d77fc00fcc2a91b41b93522e033
molBR <pedromollica@gmail.com>
2017-07-18 13:28:13 -0300

    Insercao quase pronta

--- source/boundaries/venda_produtos_menor.py
+++ source/boundaries/venda_produtos_menor.py
@@ -15,16 +15,24 @@ class Application:
     def GetWindow(self):
         return self.top
 
-    def insereTudo(self,bd,v1):
+    def insereTudo(self,bd,venda):
         print "oi"
 
-    def calculaTotal(self):
-        print "oie"
+    def calculaTotal(self,v1):
+        #try:
+            valor = float(self.valor_espaco2.get()) + float(v1.getValor_total().get())
+        #except Exception as e:
+         #   return
+        #else:
+            #self.vt = valor
+            self.v2.set(str(valor))
 
-    def FazTela(self,bd,v1):
+
+    def FazTela(self,bd,venda):
 
         if(self.top!=None):
-            return
+            self.CloseWindow()
+            self.FazTela(bd,venda)
 
         self.top = Toplevel()
         self.top.title('Guts\' Orçamento - Valor final')
@@ -35,24 +43,28 @@ class Application:
         self.valor_espaco1 = Label(self.container1, text="Valor Espaco")
         self.valor_espaco1['font'] = ['bold']
         self.valor_espaco1.grid(row=1, column=1)
-        self.valor_espaco2 = Entry(self.container1)
+        self.v1 = StringVar()
+        self.v1.set("0")
+        self.valor_espaco2 = Entry(self.container1,textvariable = self.v1)
         self.valor_espaco2["width"] = 25
         self.valor_espaco2['font'] = ['bold']
         self.valor_espaco2.grid(row=2, column=1)
         self.photo = PhotoImage(file=os.getcwd() + "/source/images/repeat.gif")
         self.calcular = Button(self.container1, width=20, height=20, image=self.photo, relief=FLAT,
-                                  command=lambda: self.calculaTotal())
+                                  command=lambda: self.calculaTotal(venda))
         self.calcular.grid(row=1, column=2)
         self.valor_final1 = Label(self.container1, text="Valor Final:")
         self.valor_final1['font'] = ['bold']
         self.valor_final1.grid(row=3, column=1)
-        self.valor_final2 = Entry(self.container1)
+        self.v2 = StringVar()
+        self.v2.set(venda.getCusto_diversos().get())
+        self.valor_final2 = Entry(self.container1,textvariable = self.v2)
         self.valor_final2["width"] = 25
         self.valor_final2['font'] = ['bold']
         self.valor_final2.grid(row=4, column=1)
         self.espaco1 = Label(self.container1, text="          ")
         self.espaco1.grid(row=5, column=2)
-        self.ok = Button(self.container1, text="OK", bg=self.cor1, command=lambda: self.insereTudo(bd,v1))
+        self.ok = Button(self.container1, text="OK", bg=self.cor1, command=lambda: self.insereTudo(bd,venda))
         self.ok['font']=['bold']
         self.ok['fg']='white'
         self.ok['padx'] = 2
