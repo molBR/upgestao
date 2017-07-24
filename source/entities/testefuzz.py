58b6bd39b5f0a2729238da775c95ebf334968171
molBR <pedromollica@gmail.com>
2017-07-24 15:22:17 -0300

    Pesquisa com acento

--- source/entities/testefuzz.py
+++ source/entities/testefuzz.py
@@ -4,9 +4,10 @@ from fuzzywuzzy import process
 
 def PassaFuzzy(listaBD,selecionado):
 	listaRetorna = []
+
+	print listaBD
 	for x in range(0,len(listaBD)):
-		print fuzz.partial_ratio(listaBD[x],selecionado)
-		if fuzz.partial_ratio(listaBD[x], selecionado)>=80:
+		if fuzz.partial_ratio(listaBD[x][1], selecionado)>=80:
 			listaRetorna.append(listaBD[x])
 	return listaRetorna
 	
